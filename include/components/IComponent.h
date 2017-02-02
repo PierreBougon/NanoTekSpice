df5daf44085ab7e8f25baaa382e12e01f7137066
PierreBougon <pierre.bougon@epitech.eu>
2017-02-02 19:08:52 +0100

    IComponent done

--- include/components/IComponent.h
+++ include/components/IComponent.h
@@ -6,10 +6,37 @@
 #define NANOTEKSPICE_ICOMPONENT_H_
 
 
-class IComponent
-{
+#include <cstddef>
 
-};
+namespace nts
+{
+    enum Tristate
+    {
+        UNDEFINED   = (-true),
+        TRUE        = true,
+        FALSE       = false
+    };
+
+    class IComponent
+    {
+    public:
+        virtual ~IComponent(){};
+
+        // Compute value of the precised pin
+        virtual nts::Tristate   Compute(size_t pin_num_this = 1) = 0;
+
+        // Useful to link IComponent together
+        virtual void            SetLink(size_t pin_num_this,
+                                        nts::IComponent &component, size_t
+                                        pin_num_target) = 0;
+
+        // Print on term the name of the component and the state of every pin of the
+        // current component
+        // The output won't be tested, but it may help you as a trace
+        virtual void            Dump(void) const = 0;
+    };
+
+}
 
 
 #endif //NANOTEKSPICE_ICOMPONENT_H_
--- src/parser/IParser.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by Pierre Bougon on 02/02/17.
-//
-
-#include "parser/IParser.h"
