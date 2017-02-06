8c517a9a6127210d0dc4e49fb4f6bd9a2ebe732e
PierreBougon <pierre.bougon@epitech.eu>
2017-02-06 16:42:24 +0100

    Signal Handler class to wrap static signal methods

---
+++ include/SignalHandler.h
@@ -0,0 +1,16 @@
+//
+// Created by Pierre Bougon on 04/02/17.
+//
+
+#ifndef NANOTEKSPICE_SIGNALHANDLER_H_
+#define NANOTEKSPICE_SIGNALHANDLER_H_
+
+
+class SignalHandler
+{
+public:
+    static void sigHandlerInt(int sig);
+};
+
+
+#endif // !NANOTEKSPICE_SIGNALHANDLER_H_
---
+++ src/SignalHandler.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Pierre Bougon on 04/02/17.
+//
+
+#include "SignalHandler.h"
+
+void SignalHandler::sigHandlerInt(int sig)
+{
+
+}
