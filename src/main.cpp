bc9f3d017dcf360691881a4021d659347fbbe054
Clement Peau <clement.peau@epitech.eu>
2017-02-14 16:17:03 +0100

    Added logic in main

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #include "parser/Parser.h"
 #include "parser/Lexer.h"
 
+void getStandardInput(nts::Parser parser);
 //TODO: FAIRE LE MAKEFILE;
 int main(int ac, char **av)
 {
@@ -18,7 +19,18 @@ int main(int ac, char **av)
     if (ac < 2)
         Logger::log(Logger::Error, "Usage: ./nanotekspice file [Options]");
     nts::Lexer::readFileAndArguments(av, parser);
+    getStandardInput(parser);
     parser.checkChipset();
     parser.checkLinks();
+	parser.parseTree(*(parser.getRoot()));
     return (0);
+}
+void getStandardInput(nts::Parser parser) {
+    std::string inputLine;
+
+    while (std::cin) {
+        getline(std::cin, inputLine);
+        std::cout << inputLine << std::endl;
+    }
+
 }
