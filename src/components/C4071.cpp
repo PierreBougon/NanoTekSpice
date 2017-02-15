a3a92deccc45bfb963c84b9b3f10f4eee549e411
PierreBougon <pierre.bougon@epitech.eu>
2017-02-15 16:56:31 +0100

    OR and AND gate done

--- src/components/C4071.cpp
+++ src/components/C4071.cpp
@@ -16,25 +16,28 @@ nts::Component::C4071::C4071(const std::string &name) : AComponent(name)
     pinList.push_back(new InputPin());
     pinList.push_back(new InputPin());
     pinList.push_back(new OutputPin(dynamic_cast<InputPin *>(pinList[3]),
-                                dynamic_cast<InputPin *>(pinList[4])));
+                                    dynamic_cast<InputPin *>(pinList[4])));
     pinList.push_back(new InputPin());
     pinList.push_back(new InputPin());
     pinList.push_back(new OutputPin(dynamic_cast<InputPin *>(pinList[6]),
-                                dynamic_cast<InputPin *>(pinList[7])));
+                                    dynamic_cast<InputPin *>(pinList[7])));
+    pinList.push_back(new InputPin());
+    pinList.push_back(new InputPin());
+    pinList.push_back(new OutputPin(dynamic_cast<InputPin *>(pinList[9]),
+                                    dynamic_cast<InputPin *>(pinList[10])));
 }
 
 nts::Tristate nts::Component::C4071::gate(nts::Component::InputPin const *inputPin1,
                                           nts::Component::InputPin const *inputPin2) const
 {
-    if (inputPin1->getState() == nts::Tristate::FALSE)
-        return (nts::Tristate::FALSE);
+    if (inputPin1->getState() == nts::Tristate::TRUE)
+        return (nts::Tristate::TRUE);
 
     if (inputPin1->getState() == nts::Tristate::UNDEFINED
         || inputPin2->getState() == nts::Tristate::UNDEFINED)
         return (nts::Tristate::UNDEFINED);
 
-    if (inputPin1->getState() == nts::Tristate::TRUE
-        && inputPin2->getState() == nts::Tristate::TRUE)
+    if (inputPin2->getState() == nts::Tristate::TRUE)
         return (nts::Tristate::TRUE);
 
     return (nts::Tristate::FALSE);
--- src/components/C4081.cpp
+++ src/components/C4081.cpp
@@ -2,15 +2,48 @@
 // Created by Pierre Bougon on 06/02/17.
 //
 
+#include <components/InputPin.h>
+#include <components/OutputPin.h>
 #include "components/C4081.h"
 
 nts::Component::C4081::C4081(const std::string &name) : AComponent(name)
-{}
+{
+    InputPin inputPin;
+
+    pinList.push_back(new InputPin());
+    pinList.push_back(new InputPin());
+    pinList.push_back(new OutputPin(dynamic_cast<InputPin *>(pinList[0]),
+                                    dynamic_cast<InputPin *>(pinList[1])));
+    pinList.push_back(new InputPin());
+    pinList.push_back(new InputPin());
+    pinList.push_back(new OutputPin(dynamic_cast<InputPin *>(pinList[3]),
+                                    dynamic_cast<InputPin *>(pinList[4])));
+    pinList.push_back(new InputPin());
+    pinList.push_back(new InputPin());
+    pinList.push_back(new OutputPin(dynamic_cast<InputPin *>(pinList[6]),
+                                    dynamic_cast<InputPin *>(pinList[7])));
+    pinList.push_back(new InputPin());
+    pinList.push_back(new InputPin());
+    pinList.push_back(new OutputPin(dynamic_cast<InputPin *>(pinList[9]),
+                                    dynamic_cast<InputPin *>(pinList[10])));
+
+}
 
 nts::Tristate nts::Component::C4081::gate(nts::Component::InputPin const *inputPin1,
                                           nts::Component::InputPin const *inputPin2) const
 {
-    return UNDEFINED;
+    if (inputPin1->getState() == nts::Tristate::FALSE)
+        return (nts::Tristate::FALSE);
+
+    if (inputPin1->getState() == nts::Tristate::UNDEFINED
+        || inputPin2->getState() == nts::Tristate::UNDEFINED)
+        return (nts::Tristate::UNDEFINED);
+
+    if (inputPin1->getState() == nts::Tristate::TRUE
+        && inputPin2->getState() == nts::Tristate::TRUE)
+        return (nts::Tristate::TRUE);
+
+    return (nts::Tristate::FALSE);
 }
 
 nts::Component::C4081::~C4081()
