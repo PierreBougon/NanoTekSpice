2741f3a534f0ffa68c971553ebb811fa7f26b021
Clement Peau <clement.peau@epitech.eu>
2017-02-16 15:05:46 +0100

    Segfault on setState, pushing to merge

--- include/parser/Parser.h
+++ include/parser/Parser.h
@@ -16,6 +16,9 @@ namespace nts {
 		virtual void        feed(std::string const &input);
 		virtual void        parseTree(t_ast_node &root);
 		virtual t_ast_node  *createTree();
+
+		IComponent *getItemFromList(std::string lexmeValue);
+		void *getNode(ASTNodeType type, std::string string, std::vector<s_ast_node *> *actualLeaf);
 		void checkChipset();
 		void checkLinks();
 		t_ast_node *getRoot() const;
@@ -24,14 +27,12 @@ namespace nts {
 		std::vector<IComponent *>	_componentList;
 
 		bool checkIfKeywordOrComponent(std::string toBeChecked);
-		void *getNode(ASTNodeType type, std::string string, std::vector<s_ast_node *> *actualLeaf);
 		void addChipsetNode(std::string word1, std::string word2);
 		void getWords(const std::string &input, int state);
 		void addLinkNode(std::string word1, std::string word2);
 		void createAndPushANewNode(ASTNodeType section, std::string lexme, std::string value, ASTNodeType sectionTarget, std::string lexmeTarget);
 		void createListOfComponents();
 		void linkEveryComponent();
-		IComponent *getItemFromList(std::string lexmeValue);
 	};
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -6,50 +6,71 @@
 #include <utils/Logger.h>
 #include <signal.h>
 #include <SignalHandler.h>
+#include <components/CInput.h>
 #include "utils/FileHandler.h"
 #include "parser/Parser.h"
 #include "parser/Lexer.h"
 
 void exit() {
-
+	printf("exit\n");
 }
 
 void display() {
-
+	printf("display\n");
 }
 
 void simulate() {
-
+	printf("simulate\n");
 }
 
 void input() {
-
+	printf("input\n");
 }
 
 void loop() {
-
+	printf("loop\n");
 }
 
 void dump() {
-
+	printf("dump\n");
 }
 
-void createFunctionMap(std::map<std::string, void (*)()> map) {
+void createFunctionMap(std::map<std::string, void (*)()> &map) {
 	map["exit"] = &exit;
 	map["display"] = &display;
 	map["dump"] = &dump;
 	map["loop"] = &loop;
-	map["input"] = &input;
 	map["simulate"] = &simulate;
+	map["exit"]();
 }
 
 void setInput(std::string string, nts::Parser parser, unsigned long equalPos) {
+	std::string 				componentName, value;
+	nts::Component::CInput		*getter;
+	nts::Tristate				tristate;
+	long						tmp = 0;
 
+	componentName = string.substr(0, equalPos);
+	if ((value = string.substr(equalPos + 1)).empty()) {
+		Logger::log(Logger::Warning, "Please set a correct value for input", false);
+		return;
+	}
+	tmp = std::stol(value);
+	getter = (nts::Component::CInput *)parser.getItemFromList(componentName);
+	getter->setState((tmp > 0 ? nts::Tristate::TRUE : nts::Tristate::FALSE));
 }
 
 void executeCommand(std::string input, std::map<std::string, void (*)()> map, nts::Parser parser) {
 	if (input.find("=") != input.npos) {
-		setInput(input.substr(input.find("=")), parser, input.find("="));
+		setInput(input, parser, input.find("="));
+	}
+	else {
+		try {
+			map.at(input)();
+		}
+		catch (std::out_of_range e) {
+			Logger::log(Logger::Warning, "Command \"" + input + "\" is not regocnized, check for typos", false);
+		}
 	}
 }
 
