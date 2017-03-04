41871e512d1f215f36ba55aeba35340bc48ae1e6
PierreBougon <pierre.bougon@epitech.eu>
2017-03-04 18:58:53 +0100

    Invert component done

--- src/components/C4069.cpp
+++ src/components/C4069.cpp
@@ -2,18 +2,37 @@
 // Created by Pierre Bougon on 06/02/17.
 //
 
+#include "components/pins/OneEntryOutputPin.h"
+#include "components/Gates.h"
 #include "components/C4069.h"
 
 nts::Component::C4069::C4069(const std::string &name) : AComponent(name)
-{}
+{
+    InputPin *tmp;
+
+    pinList.push_back(new InputPin());
+    pinList.push_back(new OneEntryOutputPin(dynamic_cast<InputPin *>(pinList[0])));
+    pinList.push_back(new InputPin());
+    pinList.push_back(new OneEntryOutputPin(dynamic_cast<InputPin *>(pinList[2])));
+    pinList.push_back(new InputPin());
+    pinList.push_back(new OneEntryOutputPin(dynamic_cast<InputPin *>(pinList[4])));
+    pinList.push_back(nullptr);
+    tmp = new InputPin();
+    pinList.push_back(new OneEntryOutputPin(tmp));
+    pinList.push_back(tmp);
+    tmp = new InputPin();
+    pinList.push_back(new OneEntryOutputPin(tmp));
+    pinList.push_back(tmp);
+    tmp = new InputPin();
+    pinList.push_back(new OneEntryOutputPin(tmp));
+    pinList.push_back(tmp);
+}
 
 nts::Tristate nts::Component::C4069::gate(nts::Component::InputPin const *inputPin1,
                                           nts::Component::InputPin const *inputPin2) const
 {
-    return UNDEFINED;
+    return nts::Gate::invertGate(inputPin1->getState());
 }
 
 nts::Component::C4069::~C4069()
-{
-
-}
+{}
