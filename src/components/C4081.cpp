1a7915cc809e1df47b10b256382db879e163ecb4
Clement Peau <clement.peau@epitech.eu>
2017-03-04 17:05:21 +0100

    Corrected the pin order on C4081.cpp

--- src/components/C4081.cpp
+++ src/components/C4081.cpp
@@ -9,25 +9,24 @@
 
 nts::Component::C4081::C4081(const std::string &name) : AComponent(name)
 {
-    InputPin inputPin;
-
     pinList.push_back(new InputPin());
     pinList.push_back(new InputPin());
     pinList.push_back(new OutputPin(dynamic_cast<InputPin *>(pinList[0]),
                                     dynamic_cast<InputPin *>(pinList[1])));
     pinList.push_back(new InputPin());
     pinList.push_back(new InputPin());
-    pinList.push_back(new OutputPin(dynamic_cast<InputPin *>(pinList[3]),
-                                    dynamic_cast<InputPin *>(pinList[4])));
+    pinList.insert(pinList.begin() + 3,
+                   new OutputPin(dynamic_cast<InputPin *>(pinList[3]),
+                                 dynamic_cast<InputPin *>(pinList[4])));
     pinList.push_back(new InputPin());
     pinList.push_back(new InputPin());
     pinList.push_back(new OutputPin(dynamic_cast<InputPin *>(pinList[6]),
                                     dynamic_cast<InputPin *>(pinList[7])));
     pinList.push_back(new InputPin());
     pinList.push_back(new InputPin());
-    pinList.push_back(new OutputPin(dynamic_cast<InputPin *>(pinList[9]),
-                                    dynamic_cast<InputPin *>(pinList[10])));
-
+    pinList.insert(pinList.begin() + 9,
+                   new OutputPin(dynamic_cast<InputPin *>(pinList[9]),
+                                 dynamic_cast<InputPin *>(pinList[10])));
 }
 
 nts::Tristate nts::Component::C4081::gate(nts::Component::InputPin const *inputPin1,
